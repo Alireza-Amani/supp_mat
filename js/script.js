954987e0993eefe301a4f3b13d2e8ebfa527b763
Alireza Amani <alireza.amani101@gmail.com>
2022-02-09 20:23:03 -0500

    Hide on blur - data section

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
 // Event handling
 document.addEventListener("DOMContentLoaded", function (event) {
-  document.querySelector("#data_show_button").addEventListener("click", function () {
-      console.log("Plotted")
-      Plotly.newPlot("map_plotly", data, layout)
+  document
+    .querySelector("#data_show_button")
+    .addEventListener("click", function () {
+      console.log("Plotted");
+      Plotly.newPlot("map_plotly", data, layout);
+    });
+
+    $("#data_complete").focusout(function () {
+    console.log("#data_complete lost focus!");
+    $("#data_section").collapse("hide");
   });
+  
+});
+
+// JQuerry
+$(document).ready(function () {
+  console.log("JQuery speaking");
+
+  // $("#data_show_button").focus(function () {
+  //   console.log("data button focused!");
+  // });
+
+  // $("#data_complete").focusout(function () {
+  //   console.log("#data_complete lost focus!");
+  //   $("#data_section").collapse("hide");
+  // });
 });
